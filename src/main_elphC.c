99539946fceb16374841a0104d96ced52a9d6a3e
muralidhar-nalabothula <muralidharrsvm7@gmail.com>
2024-03-18 08:08:20 +0100

    Print report to stdout

--- src/main_elphC.c
+++ src/main_elphC.c
@@ -1,6 +1,6 @@
 /*
  * This is the starting point of the program
-*/
+ */
 
 #include "elphC.h"
 #include "preprocessor/preprocessor.h"
@@ -14,11 +14,11 @@ int main(int argc, char* argv[])
 #else
     MPI_Init(&argc, &argv);
 #endif
-    
+
     int my_rank;
     MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
-    
-    struct calc_details * calc_info = malloc(sizeof(struct calc_details)); 
+
+    struct calc_details* calc_info = malloc(sizeof(struct calc_details));
 
     ND_int dft_code_tmp;
     // this is used to broad cast the enum
@@ -26,7 +26,7 @@ int main(int argc, char* argv[])
     if (my_rank == 0)
     {
         ELPH_cli_parser(argc, argv, calc_info);
-        
+
         dft_code_tmp = calc_info->code;
 
         if (calc_info->calc == CALC_ELPH)
@@ -34,18 +34,18 @@ int main(int argc, char* argv[])
             run_elph_calc = true;
         }
     }
-    
-    MPI_Bcast(&dft_code_tmp,  1, ELPH_MPI_ND_INT,    0, MPI_COMM_WORLD);
+
+    MPI_Bcast(&dft_code_tmp, 1, ELPH_MPI_ND_INT, 0, MPI_COMM_WORLD);
     MPI_Bcast(&run_elph_calc, 1, MPI_C_BOOL, 0, MPI_COMM_WORLD);
     MPI_Bcast(calc_info->input_file, sizeof(calc_info->input_file), MPI_CHAR, 0, MPI_COMM_WORLD);
-    
+
     calc_info->code = dft_code_tmp;
-    
+
     if (run_elph_calc)
     {
         elph_driver(calc_info->input_file, calc_info->code, MPI_COMM_WORLD);
     }
-    else 
+    else
     {
         // run preprocessor
         if (my_rank == 0)
@@ -74,8 +74,3 @@ int main(int argc, char* argv[])
 
     return 0;
 }
-
-
-
-
-
