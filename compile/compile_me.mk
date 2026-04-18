babc61f537fd1ede1dda1bbf1dc7d8a82f343c89
Riccardo Reho <riccardoreho95@gmail.com>
2026-04-18 09:31:50 +0200

    compilation from y6 add new files and upate make

---
+++ compile/compile_me.mk
@@ -0,0 +1,10 @@
+#
+# License-Identifier: GPL
+#
+# Copyright (C) 2025 The Yambo Team
+#
+# Authors (see AUTHORS file for details): AM MN
+#
+# LetzElPhC ep plugin - yambo build system stub
+# Integration handled via SERVICES_LIBS in libraries.mk
+#
---
+++ compile/targets.mk
@@ -0,0 +1,12 @@
+#
+# License-Identifier: GPL
+#
+# Copyright (C) 2025 The Yambo Team
+#
+# Authors (see AUTHORS file for details): AM MN
+#
+#
+# Co-Authors (see AUTHORS file for details): RR
+# LetzElPhC ep plugin - yambo build system stub
+# Integration handled via SERVICES_LIBS in libraries.mk
+#
