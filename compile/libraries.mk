6bfb2e7b8da19920f5c2cc846d6ed9330e610452
Andrea Marini <andrea.marini@cnr.it>
2026-04-29 09:54:42 +0200

    MODIFIED *  compile/libraries.mk
    Patch sent by:  Andrea Marini <andrea.marini@cnr.it>

--- compile/libraries.mk
+++ compile/libraries.mk
@@ -10,6 +10,6 @@ ifeq ($(wildcard compile/global/defs.mk),compile/global/defs.mk)
 endif
 #
 SERVICES_LIBS += [Sep,common/kdtree] [Sep,phonon] [Sep,common/ELPH_hash_map] [Sep,interpolation] 
-SERVICES_LIBS += [Sep,preprocessor] [Sep,common/cwalk] [Sep,fft] [Sep,symmetries] [Sep,wfc] 
-SERVICES_LIBS += [Sep,common] [Sep,dvloc] [Sep,parser/inih] [Sep,parser] [Sep,io/ezxml] [Sep,io/qe] [Sep,nonloc] [Sep,io] [Sep,elph]
+SERVICES_LIBS +=  [Sep,common/cwalk] [Sep,fft] [Sep,symmetries] [Sep,wfc] 
+SERVICES_LIBS += [Sep,common] [Sep,preprocessor] [Sep,dvloc] [Sep,parser/inih] [Sep,parser] [Sep,io/ezxml] [Sep,io/qe] [Sep,nonloc] [Sep,io] [Sep,elph]
 #
