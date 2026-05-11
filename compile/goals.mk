21295a62792b9e2c13cd4eab7f249cd9bedfcd3e
Riccardo Reho <riccardoreho95@gmail.com>
2026-05-11 22:01:44 +0200

    add option make lelphc make cleean_lelphc

--- compile/goals.mk
+++ compile/goals.mk
@@ -3,9 +3,7 @@
 #
 # Copyright (C) 2025 The Yambo Team
 #
-# Authors (see AUTHORS file for details): AM MN
+# Authors (see AUTHORS file for details): AM MN RR
 #
-# Co-Authors (see AUTHORS file for details): RR
-# LetzElPhC ep plugin - yambo build system stub
-# Integration handled via SERVICES_LIBS in libraries.mk
-#
+
+.PHONY: lelphc clean_lelphc
--- compile/targets.mk
+++ compile/targets.mk
@@ -3,10 +3,15 @@
 #
 # Copyright (C) 2025 The Yambo Team
 #
-# Authors (see AUTHORS file for details): AM MN
+# Authors (see AUTHORS file for details): AM MN RR
 #
-#
-# Co-Authors (see AUTHORS file for details): RR
-# LetzElPhC ep plugin - yambo build system stub
-# Integration handled via SERVICES_LIBS in libraries.mk
-#
+# Build the standalone lelphc binary from  LetzElPhC C source.
+# Requires yambo to have been configured first (config/setup must exist).
+# Usage: make lelphc          -> builds plugins/ep/services/lelphc
+#        make clean_lelphc    -> cleans the standalone build
+
+lelphc:
+	$(MAKE) -C $(srcdir)/plugins/ep/services
+
+clean_lelphc:
+	-$(MAKE) -C $(srcdir)/plugins/ep/services clean
