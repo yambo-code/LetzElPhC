693bc3f489e854babebf7695e68c8ac3248b8734
Riccardo Reho <riccardoreho95@gmail.com>
2026-05-26 22:02:47 +0200

    introduce new order

--- compile/libraries.mk
+++ compile/libraries.mk
@@ -9,8 +9,28 @@ ifeq ($(wildcard compile/global/defs.mk),compile/global/defs.mk)
   include compile/defs.mk
 endif
 #
-EP_S1= [Sletz,nonloc] [Sletz,io/ezxml] [Sletz,parser/inih] [Sletz,io] 
-EP_S2= [Sletz,io/qe] [Sletz,common] [Sletz,dvloc] [Sletz,elph] [Sletz,wfc] [Sletz,symmetries] [Sletz,fft]
-EP_S3= [Sletz,common/cwalk] [Sletz,preprocessor] [Sletz,interpolation] [Sletz,common/ELPH_hash_map] [Sletz,phonon] [Sletz,common/kdtree] [Sletz,parser]
+# Library link order is INTRINSIC to LetzElPhC's dependency.
+# Static linker is left-to-right: symbol-users before symbol-providers.
 #
-SERVICES_LIBS += ${EP_S1} ${EP_S2} ${EP_S3}
+# Dependency tiers (each tier depends only on equal/lower tiers):
+#   T5 drivers      : interpolation, elph
+#   T4 QE I/O       : io/qe
+#   T3 main I/O     : io
+#   T2 kernels+pp   : dvloc, nonloc, symmetries, preprocessor
+#   T1 primitives   : wfc, fft, phonon, parser
+#   T0 base+vendors : common, common/cwalk, common/ELPH_hash_map, common/kdtree,
+#                     io/ezxml, parser/inih
+#
+# elph (T5) hosts the Fortran-to-C ABI boundary (ep_f2c_bridge.c):
+#   elph_driver_f2c / elph_driver_cb_f2c / elph_driver_cb2_f2c
+# These are the ONLY entry points yambo_ep calls into LetzElPhC.
+#
+EP_S1= [Sletz,interpolation] [Sletz,elph]
+EP_S2= [Sletz,io/qe]
+EP_S3= [Sletz,io]
+EP_S4= [Sletz,dvloc] [Sletz,nonloc] [Sletz,symmetries] [Sletz,preprocessor]
+EP_S5= [Sletz,wfc] [Sletz,fft] [Sletz,phonon] [Sletz,parser]
+EP_S6= [Sletz,common] [Sletz,common/cwalk] [Sletz,common/ELPH_hash_map] \
+        [Sletz,common/kdtree] [Sletz,io/ezxml] [Sletz,parser/inih]
+#
+SERVICES_LIBS += ${EP_S1} ${EP_S2} ${EP_S3} ${EP_S4} ${EP_S5} ${EP_S6}
